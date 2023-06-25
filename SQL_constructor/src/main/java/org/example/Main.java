a52dd12cdacd988e708e7372b503c8f8d9b20fc7
1837980639 <106661593+1837980639@users.noreply.github.com>
2023-06-25 08:00:34 +0800

    更新 Main.java

--- SQL_constructor/src/main/java/org/example/Main.java
+++ SQL_constructor/src/main/java/org/example/Main.java
@@ -9,6 +9,6 @@ import org.example.function.util.sql_create;
 public class Main {
     public static void main(String[] args) {
         serveMain serveMain = new serveMain();
-        serveMain.Sql_create_main("D:\\桌面\\新增", "DWD_SXBL_SHSYJ_TABLE", 103);
+        serveMain.Sql_create_main("D:\\桌面\\新增", "DWD_SXBL_SHSYJ_TABLE", 1);
     }
 }
