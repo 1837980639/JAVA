f382097f1089ab4eca9c196dd056c35c5f2c325d
作死的老妖精 <106661593+1837980639@users.noreply.github.com>
2023-07-27 20:31:24 +0800

    异或

--- demoTest/src/Main.java
+++ demoTest/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
        boolean flag = true;
        boolean fk = false;
+       // 异或
        if(fk^flag){
            System.out.println(12);
        }
